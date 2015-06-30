8cb7ff0496720f803f92e94669b1c5d958137fe1
Andreas <admin@az-core.com>
2015-06-30 17:13:29 +0700

    skip prototype
    langsung development aja deh...

--- js/script.js
+++ js/script.js
@@ -1,2 +1,14 @@
 // ganti menu jadi center
 $('#menu-centar').position('ready', )
+//Update - position
+$(document).ready(function() {
+ 
+$("#owl-demo").owlCarousel({
+navigation : true
+});
+    
+     $("#owl-demo2").owlCarousel({
+navigation : true
+});
+ 
+});
